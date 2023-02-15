faddc25e774c2dd81d7d83b937d092f2ba39347f
samolin <samolin.vlad@gmail.com>
2023-02-15 13:53:12 +0300

    register user page and set home page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+from .forms import UserForm
+from .models import User
+
+
+def registerUser(request):
+    if request.method == 'POST':
+        print(request.POST)
+        form = UserForm(request.POST)
+        if form.is_valid():
+            password = form.cleaned_data['password']
+            user = form.save(commit=False)
+            user.set_password(password)
+            user.role = User.CUSTOMER
+            user.save()
+            messages.success(request, 'Account has been created successfully! ')
+            return redirect('registerUser')
+    else:
+        form = UserForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/registerUser.html', context)
